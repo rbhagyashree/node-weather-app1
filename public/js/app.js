10167950db8603d88d8a9200defe883d76dfbef3
Bhagyashree Rajpathak <rbhagyashree@gmail.com>
2020-04-10 10:26:02 -0400

    setup app for heroku - updated url

--- public/js/app.js
+++ public/js/app.js
@@ -25,7 +25,7 @@ weatherForm.addEventListener('submit', (e) => {
 
     console.log('form submitted');
     const location = search.value;
-    const url = 'http://localhost:3000/weather?address=' + location;
+    const url = '/weather?address=' + location;
 
     fetch(url).then((response) => {
     response.json().then((data) => {
